a6058478b960da002bb8753121f330b7d636863a
Pradel Léo <pradel.leo@gmail.com>
2017-07-07 15:32:43 +0200

    Fix issue and add docs with gatsby

---
+++ docs/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Modal from './modal';
+import '../css/normalize.css';
+import '../css/stylesheet.css';
+import '../css/github-light.css';
+import '../css/style.css';
+
+export default Modal;
