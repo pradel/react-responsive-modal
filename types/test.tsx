474ffe43ba0b00e26ca39bacd7fc7c23318409ad
Pradel Léo <pradel.leo@gmail.com>
2018-04-28 19:58:30 +0200

    provide typescripts types

---
+++ types/test.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Modal = require('react-responsive-modal');
+
+const onClose = () => null;
+const open = true;
+
+function SimpleModal() {
+  return <Modal onClose={onClose} open={open} />;
+}
+
+function PropsModal() {
+  return (
+    <Modal
+      onClose={onClose}
+      open={open}
+      closeOnEsc={true}
+      center={true}
+      showCloseIcon={true}
+      closeIconSize={20}
+      animationDuration={20}
+      onEntered={() => null}
+      onExited={() => null}
+      onEscKeyDown={() => null}
+      onOverlayClick={() => null}
+      classNames={{}}
+      styles={{}}
+    />
+  );
+}
