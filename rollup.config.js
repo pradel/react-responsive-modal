0d459b9521f4fc049c78f56ee1732fb41e7be229
Pradel Léo <pradel.leo@gmail.com>
2018-05-21 12:16:23 +0200

    remove unused deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
 import pkg from './package.json';
 
