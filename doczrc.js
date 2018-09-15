69c9d3da6f21898712b99e45779ebbb8bd0f4372
Pradel Léo <pradel.leo@gmail.com>
2018-09-15 16:52:50 +0200

    docz-plugin-netlify

--- doczrc.js
+++ doczrc.js
@@ -1,10 +1,12 @@
 import { css } from 'docz-plugin-css';
+import doczPluginNetlify from "docz-plugin-netlify";
 
 export default {
   title: 'react-responsive-modal',
   description: 'A simple responsive react modal',
   src: '.',
   plugins: [
+    doczPluginNetlify(),
     css({
       preprocessor: 'postcss',
       cssmodules: true,
