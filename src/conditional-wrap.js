d50c759bbcfcd015aea3acac520871a73a18f7f8
RedTn <redmond.tran@gmail.com>
2018-11-01 21:11:15 -0700

    fix(conditionalwrap): component should always return one child

--- src/conditional-wrap.js
+++ src/conditional-wrap.js
@@ -1,4 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const ConditionalWrap = ({ condition, wrap, children }) =>
-  condition ? wrap(children) : children;
+  condition ? wrap(children) : <div>{children}</div>;
+
+ConditionalWrap.propTypes = {
+  condition: PropTypes.bool,
+  wrap: PropTypes.func,
+  children: PropTypes.node
+}
+
+ConditionalWrap.defaultProps = {
+  condition: false,
+  wrap: null,
+  children: null
+}
 
 export default ConditionalWrap;
