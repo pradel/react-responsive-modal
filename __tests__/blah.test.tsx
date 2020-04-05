2f6bce6401fee1220c34946abcfeb70487d90b45
pradel <pradel.leo@gmail.com>
2020-04-05 15:35:23 +0200

    Add @testing-library/react

--- __tests__/blah.test.tsx
+++ __tests__/blah.test.tsx
@@ -1,11 +1,36 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { render, fireEvent, screen } from '@testing-library/react';
+
+function HiddenMessage({ children }: any) {
+  const [showMessage, setShowMessage] = React.useState(false);
+  return (
+    <div>
+      <label htmlFor="toggle">Show Message</label>
+      <input
+        id="toggle"
+        type="checkbox"
+        onChange={(e) => setShowMessage(e.target.checked)}
+        checked={showMessage}
+      />
+      {showMessage ? children : null}
+    </div>
+  );
+}
 
 describe('it', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  test('shows the children when the checkbox is checked', () => {
+    const testMessage = 'Test Message';
+    render(<HiddenMessage>{testMessage}</HiddenMessage>);
+
+    // query* functions will return the element or null if it cannot be found
+    // get* functions will return the element or throw an error if it cannot be found
+    expect(screen.queryByText(testMessage)).toBeNull();
+
+    // the queries can accept a regex to make your selectors more resilient to content tweaks and changes.
+    fireEvent.click(screen.getByLabelText(/show/i));
+
+    // .toBeInTheDocument() is an assertion that comes from jest-dom
+    // otherwise you could use .toBeDefined()
+    expect(screen.getByText(testMessage)).toBeInTheDocument();
   });
 });
--- __tests__/setupTests.js
+++
@@ -1,7 +0,0 @@
-import 'babel-polyfill';
-import 'jest-enzyme';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-window.scroll = jest.fn();
