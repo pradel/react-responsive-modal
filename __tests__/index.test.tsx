3d909b6c90261e6bd1a40de0a522ac4f85a487a8
pradel <pradel.leo@gmail.com>
2020-11-08 01:24:56 +0100

    test: test that scroll is not blocked when blockScroll is false

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -339,6 +339,17 @@ describe('modal', () => {
     });
   });
 
+  describe('prop: blockScroll', () => {
+    it('should not block the scroll when modal is opened and blockScroll is false', () => {
+      render(
+        <Modal open blockScroll={false} onClose={() => null}>
+          <div>modal content</div>
+        </Modal>
+      );
+      expect(document.documentElement.style.position).toBe('');
+    });
+  });
+
   describe('prop: onEscKeyDown', () => {
     it('should be called when esc key is pressed', async () => {
       const onEscKeyDown = jest.fn();
