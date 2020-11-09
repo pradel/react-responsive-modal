cfcf5df675fb3b889945d05e633cd7fa9207483d
pradel <pradel.leo@gmail.com>
2020-11-09 14:07:22 +0100

    test multiple modals and esc key

--- cypress/integration/modal.spec.ts
+++ cypress/integration/modal.spec.ts
@@ -12,6 +12,13 @@ describe('simple modal', () => {
     cy.get('[data-testid=modal]').should('exist');
   });
 
+  // TODO overlay not working, see how to fix
+  // it('should close modal when clicking overlay', () => {
+  //   cy.get('button').eq(2).click();
+  //   cy.get('[data-testid=overlay]').click();
+  //   cy.get('[data-testid=modal]').should('not.exist');
+  // });
+
   it('should close modal when clicking the close icon', () => {
     cy.get('button').eq(2).click();
     cy.get('[data-testid=close-button]').click();
@@ -20,6 +27,16 @@ describe('simple modal', () => {
 
   it('should close modal when pressing esc key', () => {
     cy.get('button').eq(2).click();
+    cy.get('body').type('{esc}');
+    cy.get('[data-testid=modal]').should('not.exist');
+  });
+
+  it('should close only last modal when pressing esc key when multiple modals are opened', () => {
+    cy.get('button').eq(8).click();
+    cy.get('[data-testid=modal] button').eq(0).click();
+    cy.get('[data-testid=modal]').should('have.length', 2);
+    cy.get('body').type('{esc}');
+    cy.get('[data-testid=modal]').should('have.length', 1);
     cy.get('body').type('{esc}');
     cy.get('[data-testid=modal]').should('not.exist');
   });
