d2d80f5eef4cc59440f8ce966ba24d86b6dd7b75
pradel <pradel.leo@gmail.com>
2020-11-09 13:44:37 +0100

    Fix tests

--- cypress/integration/modal.spec.ts
+++ cypress/integration/modal.spec.ts
@@ -3,6 +3,8 @@
 describe('simple modal', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/examples');
+    // Page is heavy to load so we wait for it to be loaded
+    cy.wait(500);
   });
 
   it('should open modal when clicking open button', () => {
@@ -13,8 +15,6 @@ describe('simple modal', () => {
   it('should close modal when clicking the close icon', () => {
     cy.get('button').eq(2).click();
     cy.get('[data-testid=close-button]').click();
-    // Wait for animation
-    cy.wait(450);
     cy.get('[data-testid=modal]').should('not.exist');
   });
 });
