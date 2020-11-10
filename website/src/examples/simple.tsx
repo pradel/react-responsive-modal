48b2018e55cb01e5ef33af9afbc8a5a2cf33eabf
pradel <pradel.leo@gmail.com>
2020-11-10 14:29:59 +0100

    simple modal example

---
+++ website/src/examples/simple.tsx
@@ -0,0 +1,15 @@
+() => {
+  const [open, setOpen] = React.useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  return (
+    <div>
+      <button onClick={onOpenModal}>Open modal</button>
+      <Modal open={open} onClose={onCloseModal} center>
+        <h2>Simple centered modal</h2>
+      </Modal>
+    </div>
+  );
+};
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,3 +1,27 @@
-const IndexPage = () => <div className="container px-4">Hello</div>;
+import preval from 'babel-plugin-preval/macro';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+
+const file = preval`
+const fs = require('fs');
+const path = require('path');
+module.exports = fs.readFileSync(require.resolve(path.join(__dirname, '../examples/simple.tsx')), 'utf8');
+`;
+
+console.log(file);
+
+const IndexPage = () => (
+  <div className="container px-4">
+    Hello
+    <div>
+      <LiveProvider code={file} scope={{ Modal }}>
+        <LiveEditor />
+        <LiveError />
+        <LivePreview />
+      </LiveProvider>
+    </div>
+  </div>
+);
 
 export default IndexPage;
