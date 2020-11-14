4f02e51f2081c3f5af2405855fdcd930e6bb8656
pradel <pradel.leo@gmail.com>
2020-11-14 17:28:31 +0100

    add fathom

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import Router from 'next/router';
+import * as Fathom from 'fathom-client';
 import 'typeface-inter';
 import 'react-responsive-modal/styles.css';
 import '../examples/custom-styling.css';
@@ -7,7 +10,19 @@ import '../examples/custom-animation.css';
 import '../styles/atom-one-light.css';
 import '../styles/index.css';
 
+// Record a pageview when route changes
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
+  // Initialize Fathom when the app loads
+  useEffect(() => {
+    Fathom.load('PIMHMGXF', {
+      excludedDomains: ['localhost'],
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
