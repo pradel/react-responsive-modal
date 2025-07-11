f268edf7248f9d90c0f8061dc874350d56ad1f03
Léo Pradel <pradel.leo@gmail.com>
2025-07-11 19:01:38 +0200

    publint

--- react-responsive-modal/tsdown.config.ts
+++ react-responsive-modal/tsdown.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
   entry: 'src/index.tsx',
   format: ['esm', 'cjs'],
+  platform: 'browser',
+  exports: true,
+  sourcemap: true,
+  publint: true,
 });
