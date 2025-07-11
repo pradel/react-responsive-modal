1bbe5bf2b8e5f28faab8effb118347328bef9ca4
Léo Pradel <pradel.leo@gmail.com>
2025-07-11 19:34:34 +0200

    buncle

--- react-responsive-modal/vitest.config.ts
+++ react-responsive-modal/vitest.config.ts
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
+import { codecovVitePlugin } from '@codecov/vite-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Must be at the end of the list
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: 'react-responsive-modal',
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
