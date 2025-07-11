78bf7e3eb0a46e1ea935d7d674a4dd3a3fce7901
Léo Pradel <pradel.leo@gmail.com>
2025-07-11 19:52:55 +0200

    start migrating website

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
       center: true,
