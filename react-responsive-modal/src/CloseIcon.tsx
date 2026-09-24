b43ca5b40e9e99297715ef2eca75fd9bab444001
Léo Pradel <pradel.leo@gmail.com>
2026-09-24 13:53:32 +0200

    feat: remove classnames dependency (#571)
    Replace the classnames package with a local cx helper in utils,
    removing a runtime dependency and reducing the bundle size.
    Also tighten the size-limit budgets to lock in the reduction.

--- react-responsive-modal/src/CloseIcon.tsx
+++ react-responsive-modal/src/CloseIcon.tsx
@@ -1,5 +1,6 @@
-import cx from 'classnames';
 import React from 'react';
+
+import { cx } from './utils';
 
 interface CloseIconProps {
   id?: string;
--- react-responsive-modal/src/utils.ts
+++ react-responsive-modal/src/utils.ts
@@ -1,1 +1,4 @@
 export const isBrowser = typeof window !== 'undefined';
+
+export const cx = (...classes: (string | false | null | undefined)[]) =>
+  classes.filter(Boolean).join(' ');
